699ad1a8e5e87bfee525623c50b348b232d3f9c0
AlejandraDominguezCereijo <adominguezcereijo@danielcastelao.org>
2023-03-21 12:03:33 +0100

    Ejercicio completado

--- src/Ejercicio2/Ej2.java
+++ src/Ejercicio2/Ej2.java
@@ -7,12 +7,7 @@ import java.util.ArrayList;
 public class Ej2 {
     public String pruebaEjercicio2(String ruta){
         ArrayList<Character>lista_caracteres=new ArrayList<Character>();
-        lista_caracteres.add('P');
-        lista_caracteres.add('R');
-        lista_caracteres.add('U');
-        lista_caracteres.add('E');
-        lista_caracteres.add('B');
-        lista_caracteres.add('A');
+
 
         FileReader lector;
         try{
@@ -20,6 +15,7 @@ public class Ej2 {
             int caracter= lector.read();
             while (caracter!=-1){
                 char leer= (char) caracter;
+                lista_caracteres.add((char)caracter);
                 System.out.print(leer);// al poner solo print nos lee el texto en horizontal
                 caracter=lector.read();
             }
@@ -33,7 +29,7 @@ public class Ej2 {
         for (Character c:lista_caracteres){
             builder.append(c);
         }
-        textoLeido
-        return "";
+        String textoLeido=builder.toString();
+        return textoLeido;
     }
 }
